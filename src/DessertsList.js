5476a2cebb3c16fe95d365b22f67aacb603e847a
Enkhzaya Munkh-Ochir <jini.zaya@gmail.com>
2023-09-17 17:10:47 -0500

    creating dessert list

---
+++ src/DessertsList.js
@@ -0,0 +1,12 @@
+function DessertsList(props) {
+    // Implement the component here.
+    // console.log(props)
+    // const listItems = props.map(desert => {
+    //   const itemtext = `${desert.name} - ${desert.calories} cal`
+    //   return <li>{ itemtext}</li>
+    // })
+  // return <ul> { listItems}  </ul>;
+  return "hello"
+  }
+
+  export default DessertsList;

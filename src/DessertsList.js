dcdab4e036c43518710dc0a916deed637bd9a0c4
Enkhzaya Munkh-Ochir <jini.zaya@gmail.com>
2023-09-17 17:13:22 -0500

    dessert list fixed

--- src/DessertsList.js
+++ src/DessertsList.js
@@ -1,12 +1,9 @@
 function DessertsList(props) {
-    // Implement the component here.
-    // console.log(props)
-    // const listItems = props.map(desert => {
-    //   const itemtext = `${desert.name} - ${desert.calories} cal`
-    //   return <li>{ itemtext}</li>
-    // })
-  // return <ul> { listItems}  </ul>;
-  return "hello"
+    const listItems = props.data.map(desert => {
+      const itemtext = `${desert.name} - ${desert.calories} cal`
+      return <li>{itemtext}</li>
+    })
+  return <ul> { listItems}  </ul>;
   }
 
   export default DessertsList;
